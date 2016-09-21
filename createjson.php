1ce77e49f2805a13903e0339598e12e75905d07d
Maksym Marko <maximus21@mail.ru>
2016-09-21 21:23:59 +0300

    Added the ability to create a file in JSON format with information about the points on the map

--- createjson.php
+++ createjson.php
@@ -1,34 +1,59 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 
 class CreateJSON{
 
 	function InspectionFile( $JSONfile ){
 
-		if( !file_exists( $JSONfile ) ):
+		if( file_exists( $JSONfile ) ):
 
 			$generate_file = fopen( $JSONfile, 'w' );
 
+			
 		endif;
 
 	}
 
 	function OpenJSONfile( $JSONfile, $JSONdata ){
 
-		$this->JSONdata = $JSONdata;
-
-		$openJSONfile = fopen( $JSONfile, 'a' );
+		$openJSONfile = fopen( $JSONfile, 'w+' );
 		$addJSONdata = fwrite( $openJSONfile, $JSONdata );
 		fclose( $openJSONfile );
 
 	}
 
-}
+} ?>
 
-$createJSONdata = new CreateJSON;
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Create json</title>
+	</head>
+	<body>
 
-$JSONfile = 'JSON/data.json';
-$JSONdata = '';
+		<?php
+		if( isset( $_POST['createjson'] ) ):
 
-$createJSONdata->InspectionFile( $JSONfile );
-$createJSONdata->OpenJSONfile( $JSONfile, $JSONdata );
+			$JSONfile = 'json/data.json';
+			$JSONdata = $_POST['jsondata'];
 
+			$createJSONdata = new CreateJSON;
+
+			$createJSONdata->InspectionFile( $JSONfile );
+			$createJSONdata->OpenJSONfile( $JSONfile, $JSONdata );
+
+			echo '<h1>Файл JSON создано. Расположение: "' . $JSONfile . '". ВНИМАНИЕ! При следующим сохранении, файл будет перезаписан.</h1>';
+
+			echo "
+				<script>
+					window.onload = function(){
+						setTimeout( function(){ window.location.href = 'index.php'; }, 5000 );
+					};
+				</script>
+			";
+
+		endif;
+		?>
+
+	</body>
+</html>
